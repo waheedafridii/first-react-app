34036527e8c6445319a593a14855eb98c2c2878c
waheed afridi <waheed.afridi@monet-online.com>
2020-11-14 00:31:10 +0500

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import './Hello.css';
+
+function Hello({firstName}) {
+  return (
+  <p className="myname">This is paragraph from Hello Tag name:{firstName}</p>
+  );
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <div><App name="Waheed Afridi" age={27} isReactDeveloper={true}/></div>,
   document.getElementById('root')
 );
 
